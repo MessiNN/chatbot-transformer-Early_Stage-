d93e9fb06911b8d9c227cd567fca9c4a4b9b8d59
I guess <123908647+MessiNN@users.noreply.github.com>
2024-02-14 22:48:56 +0000

    Update preprocess.py
    Torch implementation with slight changes.

--- preprocess.py
+++ preprocess.py
@@ -1,88 +1,144 @@
-from collections import Counter
-import json
+class Library:
+    def __init__(self):
+        self.name = "Dataset"
+        self.trimmed = False
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {PAD_token: "PAD", SOS_token: "SOS", EOS_token: "EOS"}
+        self.num_words = 3
 
-corpus_movie_conv = 'cornell movie-dialogs corpus/movie_conversations.txt'
-corpus_movie_lines = 'cornell movie-dialogs corpus/movie_lines.txt'
-max_len = 25
+    def addSentence(self, sentence):
+        for word in sentence.split(' '):
+            self.addWord(word)
 
+    def addWord(self, word):
+        if word not in self.word2index:
+            self.word2index[word] = self.num_words
+            self.word2count[word] = 1
+            self.index2word[self.num_words] = word
+            self.num_words += 1
+        else:
+            self.word2count[word] += 1
 
-with open(corpus_movie_conv, 'r') as c:
-    conv = c.readlines()
+    def trim(self, min_count):
+        if self.trimmed:
+            return
+        self.trimmed = True
 
-with open(corpus_movie_lines, 'r') as l:
-    lines = l.readlines()
+        keep_words = []
 
-
-
-lines_dic = {}
-for line in lines:
-    objects = line.split(" +++$+++ ")
-    lines_dic[objects[0]] = objects[-1]
-
-
-def remove_punc(string):
-    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
-    no_punct = ""
-    for char in string:
-        if char not in punctuations:
-            no_punct = no_punct + char  # space is also a character
-    return no_punct.lower()
-
-
-pairs = []
-for con in conv:
-    ids = eval(con.split(" +++$+++ ")[-1])
-    for i in range(len(ids)):
-        qa_pairs = []
-        
-        if i==len(ids)-1:
-            break
-        
-        first = remove_punc(lines_dic[ids[i]].strip())      
-        second = remove_punc(lines_dic[ids[i+1]].strip())
-        qa_pairs.append(first.split()[:max_len])
-        qa_pairs.append(second.split()[:max_len])
-        pairs.append(qa_pairs)
+        for k, v in self.word2count.items():
+            if v >= min_count:
+                keep_words.append(k)
 
 
+        print(len(keep_words), len(self.word2index), len(keep_words), len(self.word2index))
+        print('keep_words {} / {} = {:.4f}'.format(
+            len(keep_words), len(self.word2index), len(keep_words) / len(self.word2index)
+        ))
 
-word_freq = Counter()
-for pair in pairs:
-    word_freq.update(pair[0])
-    word_freq.update(pair[1])
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {PAD_token: "PAD", SOS_token: "SOS", EOS_token: "EOS"}
+        self.num_words = 3
+
+        for word in keep_words:
+            self.addWord(word)
+
+def unicodeToAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+    )
+
+def normalizeString(s):
+    s = unicodeToAscii(s.lower().strip())
+    s = re.sub(r"([.!?])", r" \1", s)
+    s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
+    s = re.sub(r"\s+", r" ", s).strip()
+    return s
+
+def readVocs(datafile):
+    pairs = []
+    df = pd.read_parquet(datafile)
+    questions = df['question'].tolist()
+    responses = df['response'].tolist()
+    for question, response in zip(questions, responses):
+      question = normalizeString(question)
+      response = normalizeString(response)
+      pair = [question, response]
+      pairs.append(pair)
+    return pairs
+
+def filterPair(p):
+    return len(p[0].split(' ')) < MAX_LENGTH and len(p[1].split(' ')) < MAX_LENGTH
+
+def filterPairs(pairs):
+    return [pair for pair in pairs if filterPair(pair)]
+
+def loadPrepareData(datafile, save_dir):
+    libra = Library()
+    pairs = readVocs(datafile)
+    pairs = filterPairs(pairs)
+    for pair in pairs:
+        libra.addSentence(pair[0])
+        libra.addSentence(pair[1])
+    return libra, pairs
 
 
+def trimRareWords(libra, pairs, MIN_COUNT):
+    libra.trim(MIN_COUNT)
+    keep_pairs = []
+    for pair in pairs:
+        input_sentence = pair[0]
+        output_sentence = pair[1]
+        keep_input = True
+        keep_output = True
+        for word in input_sentence.split(' '):
+            if word not in libra.word2index:
+                keep_input = False
+                break
+        for word in output_sentence.split(' '):
+            if word not in libra.word2index:
+                keep_output = False
+                break
 
-min_word_freq = 5
-words = [w for w in word_freq.keys() if word_freq[w] > min_word_freq]
-word_map = {k: v + 1 for v, k in enumerate(words)}
-word_map['<unk>'] = len(word_map) + 1
-word_map['<start>'] = len(word_map) + 1
-word_map['<end>'] = len(word_map) + 1
-word_map['<pad>'] = 0
+        if keep_input and keep_output:
+            keep_pairs.append(pair)
+
+    print("Trimmed from {} pairs to {}, {:.4f} of total".format(len(pairs), len(keep_pairs), len(keep_pairs) / len(pairs)))
+    return keep_pairs
+
+def indexesFromSentence(libra, sentence):
+    return [libra.word2index[word] for word in sentence.split(' ') if word in libra.word2index] + [EOS_token]
 
 
-print("Total words are: {}".format(len(word_map)))
+def zeroPadding(l, fillvalue=PAD_token):
+    padded_list = []
+    for sequence in l:
+        padded_sequence = list(sequence) + [fillvalue] * (MAX_LENGTH - len(sequence))
+        padded_list.append(padded_sequence)
+    return padded_list
 
+def inputVar(l, voc):
+    indexes_batch = [indexesFromSentence(voc, sentence) for sentence in l]
+    padList = zeroPadding(indexes_batch)
+    padVar = torch.LongTensor(padList)
+    return padVar
 
-with open('WORDMAP_corpus.json', 'w') as j:
-    json.dump(word_map, j)
+# Returns padded target sequence tensor, padding mask, and max target length
+def outputVar(l, voc):
+    indexes_batch = [indexesFromSentence(voc, sentence) for sentence in l]
+    padList = zeroPadding(indexes_batch)
+    padVar = torch.LongTensor(padList)
+    return padVar
 
-
-def encode_question(words, word_map):
-    enc_c = [word_map.get(word, word_map['<unk>']) for word in words] + [word_map['<pad>']] * (max_len - len(words))
-    return enc_c
-
-
-def encode_reply(words, word_map):
-    enc_c = [word_map['<start>']] + [word_map.get(word, word_map['<unk>']) for word in words] +     [word_map['<end>']] + [word_map['<pad>']] * (max_len - len(words))
-    return enc_c
-
-pairs_encoded = []
-for pair in pairs:
-    qus = encode_question(pair[0], word_map)
-    ans = encode_reply(pair[1], word_map)
-    pairs_encoded.append([qus, ans])
-
-with open('pairs_encoded.json', 'w') as p:
-    json.dump(pairs_encoded, p)
+def batch2TrainData(voc, pair_batch):
+    pair_batch.sort(key=lambda x: len(x[0].split(" ")), reverse=True)
+    input_batch, output_batch = [], []
+    for pair in pair_batch:
+        input_batch.append(pair[0])
+        output_batch.append(pair[1])
+    inp = inputVar(input_batch, voc)
+    output = outputVar(output_batch, voc)
+    return inp, output
